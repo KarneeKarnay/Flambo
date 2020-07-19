4d8941dc96944414180e2c875792a796938a380b
karneekarnay <mricewallace@gmail.com>
2020-07-19 17:28:45 +0100

    Initialise the Flambo Project with the LibGDX project

---
+++ desktop/src/com/karneekarny/flambo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.karneekarny.flambo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.karneekarny.flambo.FlamboMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlamboMain(), config);
+	}
+}
